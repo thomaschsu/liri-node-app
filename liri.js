dcd1abb6da8a4f56d6f0865f2a3d9e148b13cc12
Tom Hsu <thomaschsu@gmail.com>
2018-07-15 12:59:38 -0400

    Fixed env

---
+++ liri.js
@@ -0,0 +1,18 @@
+// Read and set environment variables
+require("dotenv").config();
+
+// // Imports Keys
+// var keys = require("keys");
+
+// // Accesses keys
+// var spotify = new Spotify(keys.spotify);
+// var client = new Twitter(keys.twitter);
+
+// // Sets variable command to user input
+// var command = process.argv[2];
+
+// if (command == "my-tweets") {
+//     console.log("Here are your tweets!");
+// }
+
+console.log(process.env.TWITTER_CONSUMER_KEY);
