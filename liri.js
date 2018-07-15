0b4c696c6b8dab640ea878637c7c69dc48940ac9
Tom Hsu <thomaschsu@gmail.com>
2018-07-15 15:25:18 -0400

    Finished twitter api

--- liri.js
+++ liri.js
@@ -2,18 +2,23 @@
 require("dotenv").config();
 
 // // Imports Keys
-// var keys = require("keys");
+var request = require("request");
+var keys = require("./keys");
+var Twitter = require('twitter');
 
 // // Accesses keys
 // var spotify = new Spotify(keys.spotify);
-// var client = new Twitter(keys.twitter);
+var client = new Twitter(keys.twitter);
 
-// // Sets variable command to user input
-// var command = process.argv[2];
+// Sets variable command to user input
+var command = process.argv[2];
 
-// if (command == "my-tweets") {
-//     console.log("Here are your tweets!");
-// }
-
-console.log(process.env.TWITTER_CONSUMER_KEY);
-console.log(process.env.TWITTER_CONSUMER_SECRET);
+if (command == "my-tweets") {
+    client.get('statuses/user_timeline', {user_id: 'thomashsu13'}, function(error, tweets, response) {
+        for (var i = 0; i < 20; i++) {
+            console.log("\n Tweet #" + i + " Created: " + tweets[i].created_at);
+            console.log("------------------------------------------------");
+            console.log(tweets[i].text);
+        }     
+    });
+};
