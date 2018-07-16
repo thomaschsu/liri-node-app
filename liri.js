8a538cd4cb495e70be3737f8deb9a31e1f4b89e8
Tom Hsu <thomaschsu@gmail.com>
2018-07-16 00:58:33 -0400

    Added OMDB function

--- liri.js
+++ liri.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 var keys = require("./keys");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require("request");
 
 // Accesses keys
 var spotify = new Spotify(keys.spotify);
@@ -38,15 +39,14 @@ for (var i = 3; i < commandArgs.length; i++) {
     }
 }
 
-// If there is no search, automatically search for 'the sign'
-if (query === "") {
-    search = "The Sign Ace of Base";
-} else {
-    search = query;
-}
-
 // Spotify command
 if (command == "spotify-this-song") {
+    // If there is no search, automatically search for 'the sign'
+    if (query === "") {
+        search = "The Sign Ace of Base";
+    } else {
+        search = query;
+    }
     spotify.search({
         type: 'track',
         query: search,
@@ -55,6 +55,8 @@ if (command == "spotify-this-song") {
         if (err) {
             return console.log('Error occurred: ' + err);
         } else {
+            console.log("================================================");
+            console.log("|              Spotify Command                 |");
             console.log("================================================");
             console.log("Artist: " + data.tracks.items[0].artists[0].name);
             console.log("Track: " + data.tracks.items[0].name);
@@ -67,5 +69,37 @@ if (command == "spotify-this-song") {
 
 // OMDB command
 if (command == "movie-this") {
-    console.log("This movie!");
+    var movieName = "";
+    // If no moviename then search for Mr. Nobody
+    if (movieName === "") {
+        movieName = "Mr. Nobody";
+    } else {
+        movieName = movieName;
+    }
+    // Grab words in process.argv for moviename
+    for (var i = 3; i < commandArgs.length; i++) {
+        if (i > 3 && i < commandArgs.length) {
+            movieName = movieName + "+" + commandArgs[i];
+        } else {
+            movieName += commandArgs[i];
+        }
+    }
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=8506adc6";
+    request(queryUrl, function(error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+            console.log("================================================");
+            console.log("|               OMDB Command                   |");
+            console.log("================================================");
+            console.log("\n Title: " + JSON.parse(body).Title);
+            console.log("\n Release Year: " + JSON.parse(body).Year);
+            console.log("\n IMDB Rating: " + JSON.parse(body).imdbRating);
+            console.log("\n Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+            console.log("\n Country: " + JSON.parse(body).Country);
+            console.log("\n Language: " + JSON.parse(body).Language);
+            console.log("\n Plot: " + JSON.parse(body).Plot);
+            console.log("\n Actors: " + JSON.parse(body).Actors);
+            console.log("================================================");
+        }
+    })
 };
